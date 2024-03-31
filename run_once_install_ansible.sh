449d8f62cab94a146b1d785172655a6bc736ab7b
Fouzi BOUKEZZOULA <fboukezzoula@localhost.localdomain>
2024-03-31 22:11:04 +0200

    install ansible

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -4,6 +4,10 @@ install_on_fedora() {
     sudo dnf install -y ansible
 }
 
+install_on_rhel() {
+    sudo dnf install -y ansible
+}
+
 install_on_ubuntu() {
     sudo apt-get update
     sudo apt-get install -y ansible
@@ -18,6 +22,9 @@ case "${OS}" in
     Linux*)
         if [ -f /etc/fedora-release ]; then
             install_on_fedora
+        elif [ -f /etc/os-release ]; then
+            install_on_rhel
+        else            
         elif [ -f /etc/lsb-release ]; then
             install_on_ubuntu
         else
