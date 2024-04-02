029e05200eea01f13a1e0692a5f8f6d9bce5714e
Fouzi BOUKEZZOULA <f.boukezzoula@gmail.com>
2024-04-02 02:57:53 +0200

    fix ln creation

--- dot_local/scripts/helm_install.sh
+++ dot_local/scripts/helm_install.sh
@@ -2,4 +2,3 @@ sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noa
 sudo dnf upgrade 
 sudo yum install snapd 
 sudo systemctl enable --now snapd.socket 
-sudo ln -s /var/lib/snapd/snap /snap
