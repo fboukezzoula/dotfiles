e9fbd9aa340b9843abb5bdfde3b76940e10186fa
Fouzi BOUKEZZOULA <f.boukezzoula@gmail.com>
2024-04-02 02:44:31 +0200

    fix custom scripts in playbook

--- dot_local/scripts/helm_install.sh
+++ dot_local/scripts/helm_install.sh
@@ -1,5 +1,5 @@
-dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm 
-dnf upgrade 
-yum install snapd 
-systemctl enable --now snapd.socket 
-ln -s /var/lib/snapd/snap /snap
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm 
+sudo dnf upgrade 
+sudo yum install snapd 
+sudo systemctl enable --now snapd.socket 
+sudo ln -s /var/lib/snapd/snap /snap
