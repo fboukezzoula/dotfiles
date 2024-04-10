ef8f218692447bc8363ea9494f4595b66ab3b896
Fouzi BOUKEZZOULA <f.boukezzoula@gmail.com>
2024-04-10 21:35:47 +0200

    install zsh plugins

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+PLUGINS_FOLDER="$HOME/.oh-my-zsh/plugins"
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
 	echo ".oh-my-zsh already found, skipping."
 fi
+
+# Install autosuggesions plugin
+if [ -d "$PLUGINS_FOLDER/zsh-autosuggestions" ]; then rm -Rf "$PLUGINS_FOLDER/zsh-autosuggestions"; fi
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $PLUGINS_FOLDER/zsh-autosuggestions
+
+# Install azsh-syntax-highlighting plugin
+if [ -d "$PLUGINS_FOLDER/zsh-syntax-highlighting" ]; then rm -Rf "$PLUGINS_FOLDER/zsh-syntax-highlighting"; fi
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINS_FOLDER/zsh-syntax-highlighting
+
+# Install zsh-fast-syntax-highlighting plugin
+if [ -d "$HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting" ]; then rm -Rf "$HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting"; fi
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+# Install zsh-autocomplete plugin
+if [ -d "$PLUGINS_FOLDER/zsh-autocomplete" ]; then rm -Rf "$PLUGINS_FOLDER/zsh-autocomplete"; fi
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $PLUGINS_FOLDER/zsh-autocomplete
